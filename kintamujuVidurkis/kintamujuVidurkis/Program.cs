4e3eb8d3666ac37baa1f117ad39a73560de6bbc9
Geniushas <andruis0506@gmail.com>
2020-04-06 20:47:09 +0300

    kintamujuVidurkis

---
+++ kintamujuVidurkis/kintamujuVidurkis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kintamujuVidurkis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaiciusVienas = 3;
+            double skaiciusDu = 5;
+            double skaiciusTrys = 13;
+
+            double vidurkis = (skaiciusVienas + skaiciusDu + skaiciusTrys) / 3;
+
+            Console.Write("Vidurkis: " + vidurkis);
+
+            Console.ReadLine();
+        }
+    }
+}
