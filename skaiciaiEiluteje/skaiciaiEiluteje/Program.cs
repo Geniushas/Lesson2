0dd03db4c1423ed8bdcab046e5ac6a10071870cf
Geniushas <andruis0506@gmail.com>
2020-04-09 23:51:30 +0300

    skaiciaiEiluteje

---
+++ skaiciaiEiluteje/skaiciaiEiluteje/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skaiciaiEiluteje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu:");
+            double skaicius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{skaicius} {skaicius} {skaicius} {skaicius}");
+            
+            Console.ReadLine();
+        }
+    }
+}
