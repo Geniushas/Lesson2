2afb5fc0d349e65110a61226b42889f3a3e52f90
Geniushas <andruis0506@gmail.com>
2020-04-10 00:14:52 +0300

    trueFalse

---
+++ trueFalse/trueFalse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trueFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Iveskite skaiciu A: ");
+            double skaiciusA = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Iveskite skaiciu B: ");
+            double skaiciusB = Convert.ToDouble(Console.ReadLine());
+
+            if(skaiciusA == skaiciusB)
+            {
+                Console.WriteLine("(A = B) - True");
+            }
+            else
+            {
+                Console.WriteLine("(A = B) - False");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
