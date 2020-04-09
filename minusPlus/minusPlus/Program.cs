7df4c6b38fe0aea601ed2e875089a5cbf8b806ef
Geniushas <andruis0506@gmail.com>
2020-04-10 00:19:56 +0300

    minusPlus

---
+++ minusPlus/minusPlus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minusPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Iveskite pirma skaiciu: ");
+            double skaiciusVienas = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Iveskite antra skaiciu: ");
+            double skaiciusDu = Convert.ToDouble(Console.ReadLine());
+
+            skaiciusVienas++;
+            skaiciusDu--;
+        }
+    }
+}
