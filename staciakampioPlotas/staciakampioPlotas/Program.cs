9f65a87ef9cffce59d65f9553528db6006560e0a
Geniushas <andruis0506@gmail.com>
2020-04-14 22:00:56 +0300

    staciakampioPlotas

---
+++ staciakampioPlotas/staciakampioPlotas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staciakampioPlotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double staciakampioIlgis, staciakampioPlotis;
+
+            Console.Write("Iveskite staciakampio ilgi: ");
+            staciakampioIlgis = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Iveskite staciakampio ploti: ");
+            staciakampioPlotis = Convert.ToDouble(Console.ReadLine());
+
+            double staciakampioPlotas = staciakampioIlgis * staciakampioPlotis;
+
+            Console.WriteLine("Staciakampio plotas = " + staciakampioPlotas);
+
+            Console.ReadLine();
+        }
+    }
+}
