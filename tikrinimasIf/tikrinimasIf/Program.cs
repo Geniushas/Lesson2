1a856b701ccc8405e6019765f41f2e42ec799f09
Geniushas <andruis0506@gmail.com>
2020-04-14 22:08:51 +0300

    tikrinimasIf

---
+++ tikrinimasIf/tikrinimasIf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tikrinimasIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eilute;
+
+            Console.WriteLine("Iveskite teksto eilute:");
+            eilute = Console.ReadLine();
+
+            if (eilute == "")
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
