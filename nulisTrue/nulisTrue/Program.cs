61c25d365c8e3f7e4ec99386564a43083b02ed59
Geniushas <andruis0506@gmail.com>
2020-04-14 22:13:51 +0300

    nulisTrue

---
+++ nulisTrue/nulisTrue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nulisTrue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius;
+
+            Console.Write("Iveskite skaiciu: ");
+            skaicius = Convert.ToDouble(Console.ReadLine());
+
+            if(skaicius < 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
