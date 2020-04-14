14ffa43fd2cf5e0ea20ab939391c6a87d3b4f56b
Geniushas <andruis0506@gmail.com>
2020-04-14 22:19:34 +0300

    vardasPavarde

---
+++ vardasPavarde/vardasPavarde/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vardasPavarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas, pavarde;
+
+            Console.Write("Iveskite varda: ");
+            vardas = Console.ReadLine();
+
+            Console.Write("Iveskite pavarde: ");
+            pavarde = Console.ReadLine();
+
+            Console.WriteLine(vardas + " " + pavarde);
+
+            Console.ReadLine();
+        }
+    }
+}
