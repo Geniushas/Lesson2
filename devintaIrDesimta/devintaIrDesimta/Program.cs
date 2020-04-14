af7ddb74b92819e00851a6803b93259ca1fab34d
Geniushas <andruis0506@gmail.com>
2020-04-14 22:41:36 +0300

    kazkoNepagaunuSuDatomis,JuVeiksmais

---
+++ devintaIrDesimta/devintaIrDesimta/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devintaIrDesimta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string miestoPavadinimas = "Majamis";
+            string gatvesPavadinimas = "Kalifornijos gatve";
+            string namoNumeris = "64";
+            DateTime pastatymoMetai = new DateTime(2017, 01, 01);
+            DateTime statybuPradzia = new DateTime(2015);
+            
+            Console.WriteLine(miestoPavadinimas);
+            Console.WriteLine(gatvesPavadinimas);
+            Console.WriteLine(namoNumeris);
+            Console.WriteLine(pastatymoMetai);
+            Console.ReadLine();
+        }
+    }
+}
